aa4ff91447a1169b46425a653152ce17542da557
Charis Yfantis <xyfantis@gmail.com>
2025-02-13 10:48:54 +0200

    minor updates

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -4,8 +4,15 @@ return {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      opts.skip_ssl_verification = true
       table.insert(opts.ensure_installed, 'http')
     end,
   },
+  init = function()
+    vim.bo.formatexpr = ''
+    vim.bo.formatprg = 'jq'
+  end,
+  config = function()
+    vim.keymap.set('n', '<leader>rr', '<cmd>Rest run<CR>', { desc = 'Run rest command' })
+    vim.keymap.set('n', '<leader>re', '<cmd>Rest env select<CR>', { desc = 'Select rest env' })
+  end,
 }
