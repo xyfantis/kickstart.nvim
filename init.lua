4e917ff56c5a5e8b8bb16286735787a7f1d9e25a
Charis Yfantis <xyfantis@gmail.com>
2025-02-20 17:58:08 +0200

    added some php config

--- init.lua
+++ init.lua
@@ -708,11 +708,12 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        php = { 'pint', 'php_cs_fixer' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
---
+++ lua/custom/plugins/neo-test.lua
@@ -0,0 +1,7 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'V13Axel/neotest-pest',
+  },
+  opts = { adapters = { 'neotest-pest' } },
+}
